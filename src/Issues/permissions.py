583c370ad3e9f5cec49863d1847f97acf0ea8b82
lisa367 <75017410+lisa367@users.noreply.github.com>
2023-09-27 08:32:16 +0200

    updated permissions

--- src/Issues/permissions.py
+++ src/Issues/permissions.py
@@ -8,14 +8,14 @@ class canAccessIssues(BasePermission):
     def has_permission(self, request, view):
         AUTHORIZED_METHODS = ["GET", "POST"]
         RESTRICTED_METHODS = ["PUT", "DELETE"]
-        project_id = request.query_params.get("project_id")
-        issue_id = request.query_params.get("issue_id")
-        
+        project_id = request.resolver_match.kwargs.get("project_id")
+        issue_id = request.resolver_match.kwargs.get("issue_id")
         
 
         is_project_member = Contributor.objects.filter(project=project_id, user=request.user)[0]
         issue_object = Issue.objects.filter(pk=issue_id)[0]
         # is_object_author = Issue.objects.filter(pk=issue_id, project=project_id, author_user_id=user_id)[0]
+
 
         if request.method in AUTHORIZED_METHODS:
             if is_project_member: 
@@ -34,10 +34,10 @@ class canAccessComments(BasePermission):
         RESTRICTED_METHODS = ["PUT", "DELETE"]
         """ project_id = request.project
         issue_id = request.issue_id """
-        project_id = request.query_params.get("project_id")
-        issue_id = request.query_params.get("issue_id")
+        project_id = request.resolver_match.kwargs.get("project_id")
+        issue_id = request.resolver_match.kwargs.get("issue_id")
 
-        is_project_member = Contributor.objects.filter(project=project_id, user=request.user,)[0]
+        is_project_member = Contributor.objects.filter(project=project_id, user=request.user,)
         issue_object = Issue.objects.filter(pk=issue_id)[0]
         # is_object_author = Issue.objects.filter(pk=issue_id, project=project_id, author_user_id=request.user)[0]
 
--- src/Projects/permissions.py
+++ src/Projects/permissions.py
@@ -2,66 +2,27 @@ from rest_framework.permissions import BasePermission
 from .models import Project, Contributor
 
 
-class isAuthor(BasePermission):
-    def has_permission(self, request, view):
-        AUTHORIZED_METHODS = ["GET", "POST"]
-        RESTRICTED_METHODS = ["PUT", "DELETE"]
-        project_id = request.project
-        contributor = Contributor.objects.filter(
-            project=project_id,
-            user=request.user,
-        )[0]
-        permission_level = contributor.permission
-
-        if request.method in AUTHORIZED_METHODS:
-            return True
-
-        if request.method in RESTRICTED_METHODS:
-            if permission_level == "isAuthor":
-                return True
-
-        return False
-
-
-class isContributor(BasePermission):
-    def has_permission(self, request, view):
-        AUTHORIZED_METHODS = ["GET", "POST"]
-        RESTRICTED_METHODS = ["PUT", "DELETE"]
-        project_id = request.project
-        contributor = Contributor.objects.filter(
-            project=project_id,
-            user=request.user,
-        )[0]
-        permission_level = contributor.permission
-
-        if request.method in AUTHORIZED_METHODS:
-            return True
-
-        if request.method in RESTRICTED_METHODS:
-            if permission_level == "isContributor":
-                return True
-
-        return False
-
-
 class ProjectAccessPermission(BasePermission):
-    def has_permission(self, request, view):
+    def has_permission(self, request, view, *args, **kwargs):
         CONTRIBUTOR_METHODS = ["GET", "POST"]
         AUTHOR_METHODS = ["PUT", "DELETE"]
-        project_id = request.project
-        contributor = Contributor.objects.filter(
+        project_id = request.resolver_match.kwargs.get("project_id")
+        print(project_id)
+        contributor_query = Contributor.objects.filter(
             project=project_id,
             user=request.user,
-        )[0]
-        permission_level = contributor.permission
+        )
+        if contributor_query:
+            contributor = contributor_query[0]
+            permission_level = contributor.permission
 
-        if request.method in CONTRIBUTOR_METHODS:
-            if permission_level in ["isAuthor", "isContributor"]:
-                return True
+            if request.method in CONTRIBUTOR_METHODS:
+                if permission_level in ["isAuthor", "isContributor"]:
+                    return True
 
-        if request.method in AUTHOR_METHODS:
-            if permission_level == "isAuthor":
-                return True
+            if request.method in AUTHOR_METHODS:
+                if permission_level == "isAuthor":
+                    return True
 
         return False
 
@@ -72,20 +33,23 @@ class ContributorAccessPermission(BasePermission):
             "GET",
         ]
         AUTHOR_METHODS = ["POST", "PUT", "DELETE"]
-        project_id = request.project
-        contributor = Contributor.objects.filter(
+        project_id = request.resolver_match.kwargs.get("project_id")
+        print(project_id)
+        contributor_query = Contributor.objects.filter(
             project=project_id,
             user=request.user,
-        )[0]
-        permission_level = contributor.permission
+        )
+        if contributor_query:
+            contributor = contributor_query[0]
+            permission_level = contributor.permission
 
-        if request.method in CONTRIBUTOR_METHODS:
-            if permission_level in ["isAuthor", "isContributor"]:
-                return True
+            if request.method in CONTRIBUTOR_METHODS:
+                if permission_level in ["isAuthor", "isContributor"]:
+                    return True
 
-        if request.method in AUTHOR_METHODS:
-            if permission_level == "isAuthor":
-                return True
+            if request.method in AUTHOR_METHODS:
+                if permission_level == "isAuthor":
+                    return True
 
         return False
 
